7f56b974d4421674f0145d2754d4517231b75a2d
LINDOMAR JOSE BATISTAO <sn1021328>
2024-10-24 14:15:39 -0300

    24/10/2024 14:15

--- front/src/pages/home/index.js
+++ front/src/pages/home/index.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css"; // Importar o estilo da página 'home'
 
 const Home = () => {
-  return (
-    <div className="home-container">
-      <h1>Bem-vindo à Home!</h1>
-      <p>Você fez login com sucesso.</p>
-    </div>
-  );
+
+    const [token, setToken] = useState(localStorage.getItem("accessToken"))
+
+    return (
+        <div className="home-container">
+            <h1>Bem-vindo à Home!</h1>
+            <p>Você fez login com sucesso.</p>
+            <div className="quad">
+                <p className="token">Seu Token {token}</p>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
